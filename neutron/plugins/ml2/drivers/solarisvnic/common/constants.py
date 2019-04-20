17b42719948c5854639dc1f3f648926526bddb14
Guillermo Adrian Molina <guillermoadrianmolina@hotmail.com>
2019-04-20 19:05:40 +0200

    Add Solaris VNIC support

---
+++ neutron/plugins/ml2/drivers/solarisvnic/common/constants.py
@@ -0,0 +1,18 @@
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+AGENT_TYPE_SOLARISVNIC = 'Solaris VNIC agent'
+VIF_TYPE_SOLARISVNIC = 'solarisvnic'
+EXTENSION_DRIVER_TYPE = 'solarisvnic'
+SOLARISVNIC_AGENT_BINARY = "neutron-solaris-agent"
---
+++ neutron/plugins/ml2/drivers/solarisvnic/mech_driver/mech_solarisvnic.py
@@ -0,0 +1,84 @@
+# Copyright (c) 2016 IBM Corp.
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from neutron_lib.api.definitions import portbindings
+from neutron_lib import constants
+from neutron_lib.plugins.ml2 import api
+from neutron_lib.agent import topics
+from oslo_log import log
+
+from neutron.plugins.ml2.drivers import mech_agent
+from neutron.plugins.ml2.drivers.solarisvnic.common \
+    import constants as s_constants
+
+LOG = log.getLogger(__name__)
+FLAT_VLAN = 0
+
+class SolarisVNICMechanismDriver(mech_agent.SimpleAgentMechanismDriverBase):
+    """Attach to networks using Solaris VNIC agent.
+
+    The SolarisVNICMechanismDriver integrates the ml2 plugin with the
+    solaris VNIC agent. Port binding with this driver requires the
+    solaris VNIC agent to be running on the port's host, and that agent
+    to have connectivity to at least one segment of the port's
+    network.
+    """
+
+    def __init__(self):
+        super(SolarisVNICMechanismDriver, self).__init__(
+            s_constants.AGENT_TYPE_SOLARISVNIC,
+            s_constants.VIF_TYPE_SOLARISVNIC,
+            {portbindings.CAP_PORT_FILTER: False})
+
+    def get_allowed_network_types(self, agent):
+        return [constants.TYPE_FLAT, constants.TYPE_VLAN]
+
+    def get_mappings(self, agent):
+        return agent['configurations'].get('interface_mappings', {})
+
+    def check_vlan_transparency(self, context):
+        """Solaris driver vlan transparency support."""
+        return False
+
+    def _get_vif_details(self, context, agent, segment):
+        network_type = segment[api.NETWORK_TYPE]
+        if network_type == constants.TYPE_FLAT:
+            vlan_id = FLAT_VLAN
+        elif network_type == constants.TYPE_VLAN:
+            vlan_id = segment[api.SEGMENTATION_ID]
+        else:
+            LOG.error("Network type not supported %s", network_type)
+
+        physical_network = segment[api.PHYSICAL_NETWORK]
+        physical_interface = self.get_mappings(agent).get(physical_network)
+        if not physical_network or not physical_interface:
+            LOG.error("No interface mappings for physical network %s",
+                      physical_network)
+        
+        vif_details = self.vif_details.copy()
+        vif_details[portbindings.VIF_DETAILS_VLAN] = str(vlan_id)
+        vif_details[portbindings.VIF_DETAILS_PHYSICAL_INTERFACE] = physical_interface
+        LOG.debug("VIF details %s ", vif_details)
+        return vif_details
+
+    def try_to_bind_segment_for_agent(self, context, segment, agent):
+        if self.check_segment_for_agent(segment, agent):
+            vif_details = self._get_vif_details(context, agent, segment)
+            LOG.debug("Solaris network vif_details added to context binding: %s",
+                      vif_details)
+            context.set_binding(segment[api.ID], self.vif_type, vif_details)
+            return True
+        return False
